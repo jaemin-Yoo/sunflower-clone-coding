0a0b06359757e260f4b14bcb20f970796f27684c
jaemin-Yoo <iamjm29@gmail.com>
2022-06-02 19:30:20 +0900

    Feat: Dagger-Hilt Annotation 추가

--- app/src/main/java/com/jaemin/sunflower_clone/GardenActivity.kt
+++ app/src/main/java/com/jaemin/sunflower_clone/GardenActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
 import com.jaemin.sunflower_clone.databinding.ActivityGardenBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GardenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
