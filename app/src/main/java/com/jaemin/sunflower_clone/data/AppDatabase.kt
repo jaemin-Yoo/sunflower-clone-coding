36391ef693b15ebd0319de583455f03aa23c63cd
jaemin-Yoo <iamjm29@gmail.com>
2022-06-07 11:09:15 +0900

    Feat: Room DB Setup

---
+++ app/src/main/java/com/jaemin/sunflower_clone/data/AppDatabase.kt
@@ -0,0 +1,47 @@
+package com.jaemin.sunflower_clone.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import com.jaemin.sunflower_clone.utilities.DATABASE_NAME
+import com.jaemin.sunflower_clone.utilities.PLANT_DATA_FILENAME
+import com.jaemin.sunflower_clone.worker.SeedDatabaseWorker
+import com.jaemin.sunflower_clone.worker.SeedDatabaseWorker.Companion.KEY_FILENAME
+
+@Database(entities = [Plant::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun plantDao(): PlantDao
+
+    companion object {
+
+        // 싱글톤 인스턴스화
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                .addCallback(
+                    object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            val request = OneTimeWorkRequestBuilder<SeedDatabaseWorker>()
+                                .setInputData(workDataOf(KEY_FILENAME to PLANT_DATA_FILENAME))
+                                .build()
+                            WorkManager.getInstance(context).enqueue(request)
+                        }
+                    }
+                )
+                .build()
+        }
+    }
+}
---
+++ app/src/main/java/com/jaemin/sunflower_clone/utilities/Constant.kt
@@ -0,0 +1,4 @@
+package com.jaemin.sunflower_clone.utilities
+
+const val DATABASE_NAME = "sunflower-db"
+const val PLANT_DATA_FILENAME = "plants.json"
