e5ca5fe7fe8ca318075290aa212dec15c02ccc03
jaemin-Yoo <iamjm29@gmail.com>
2022-06-08 17:49:07 +0900

    Feat: Navigation Directions class Setup

---
+++ app/src/main/java/com/jaemin/sunflower_clone/adapters/PlantAdapter.kt
@@ -0,0 +1,63 @@
+package com.jaemin.sunflower_clone.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.jaemin.sunflower_clone.HomeViewPagerFragmentDirections
+import com.jaemin.sunflower_clone.data.Plant
+import com.jaemin.sunflower_clone.databinding.ListItemPlantBinding
+
+/**
+ * PlantListFragment 안에있는 RecyclerView를 위한 어댑터
+ */
+
+
+class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PlantViewHolder(
+
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    class PlantViewHolder(
+        private val binding: ListItemPlantBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener {
+                binding.plant?.let { plant ->
+                    navigateToPlant(plant, it)
+                }
+            }
+        }
+
+        private fun navigateToPlant(
+            plant: Plant,
+            view: View
+        ) {
+            val direction =
+                HomeViewPagerFragmentDirections.actionViewPagerFragmentToPlantDetailFragment(
+                    plant.plantId
+                )
+            view.findNavController().navigate(direction)
+        }
+    }
+}
+
+private class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+
+    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem.plantId == newItem.plantId
+    }
+
+    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem == newItem
+    }
+}
