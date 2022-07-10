863c85617f75a1791c564ca06e208596abcb7cb0
jaemin-Yoo <iamjm29@gmail.com>
2022-07-10 14:46:38 +0900

    Design: PlantDetailFragment AppBar Setup

---
+++ app/src/main/java/com/jaemin/sunflower_clone/viewmodels/PlantDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.jaemin.sunflower_clone.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import com.jaemin.sunflower_clone.data.PlantRepository
+import javax.inject.Inject
+
+class PlantDetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    plantRepository: PlantRepository
+) {
+}
